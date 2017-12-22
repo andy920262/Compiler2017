ffa22d66e128ee974d7475384c5ae5d23f0dfcb5
EdenXu <EdenXu@xuconghede-MacBook-Air.local>
2017-12-22 18:33:45 +0800

    add epilogue

--- hw5/codegen.c
+++ hw5/codegen.c
@@ -2,8 +2,7 @@
 #include "codegen.h"
 #include <stdio.h>
 
-void codegen(AST_NODE *program)
-{
+void codegen(AST_NODE *program) {
     AST_NODE *decl_node = program->child;
     while (decl_node != NULL) {
         if (node_type(decl_node) == VARIABLE_DECL_LIST_NODE) {
@@ -14,17 +13,16 @@ void codegen(AST_NODE *program)
     }
 }
 
-void gen_function(AST_NODE *decl_node)
-{
+void gen_function(AST_NODE *decl_node) {
     AST_NODE *type_node = decl_node->child;
     AST_NODE *id_node = type_node->rightSibling;
     AST_NODE *block_node = id_node->rightSibling->rightSibling;
-    char *name = id_name(id_node)
+    char *name = id_name(id_node);
 
     /* Gen head */
     printf(".text\n");
     printf("_start_%s:\n", name);
-    
+
     /* Gen prologue */
     printf("str x30, [sp, #0]\n");
     printf("str x29, [sp, #-8]\n");
@@ -33,14 +31,20 @@ void gen_function(AST_NODE *decl_node)
     printf("ldr x30, =_frameSize_%s\n", name);
     printf("ldr w30, [x30, #0]\n");
     printf("sub sp, sp, w30\n");
-    
+
     gen_block(block_node);
-    
+
     /* TODO: Gen epilogue */
+    printf("_end_%s:\n", name);
+    printf("ldr x30, [x29, #8]\n");
+    printf("add sp, x29, #8\n");
+    printf("ldr x29, [x29, #0]\n");
+    printf("RET x30\n");
+    printf(".data\n");
+    printf("_frameSize_%s: .word\n", name); // TODO: size after '.word'
 
 }
 
-void gen_block(AST_NODE *block_node)
-{
-    
+void gen_block(AST_NODE *block_node) {
+
 }
