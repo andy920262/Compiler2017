42ab3d1c7e40bc0f1be4b175a1a59a466ebd9718
EdenXu <EdenXu@xuconghede-MacBook-Air.local>
2017-12-22 19:17:05 +0800

    add prototype of gen_stmt, etc.

--- hw5/codegen.h
+++ hw5/codegen.h
@@ -12,4 +12,8 @@ void codegen(AST_NODE *program);
 void gen_function(AST_NODE *decl_node);
 void gen_block(AST_NODE *block_node);
 void gen_statement(AST_NODE *stmt_node);
+void gen_while(AST_NODE* whileNode);
+void gen_for(AST_NODE* forNode);
+void gen_assignment(AST_NODE* assignmentNode);
+void gen_if(AST_NODE* ifNode);
 #endif
